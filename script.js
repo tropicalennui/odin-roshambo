da0aac50161e374ac8f2044690239d0dce3d25ff
Rosemary <rjingrey@gmail.com>
2022-08-23 20:26:05 +1000

    roshambo ready

--- script.js
+++ script.js
@@ -10,11 +10,54 @@ function getComputerChoice() {
 
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
 
+    let result = [];
     playerSelection = playerSelection.toLowerCase();
     computerSelection = getComputerChoice();
 
-    return playerSelection + ' ' + computerSelection;
+    if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")) {
+        result[0] = true;
+	    result[1] = 'Congratulations! ' + playerSelection + ' beats ' + computerSelection;
+    } else if (computerSelection == playerSelection) {
+        result[0] = false;
+	    result[1] = 'Its a draw, nobody wins...';
+    } else if (playerSelection != "rock" && playerSelection != "paper" & playerSelection != "scissors") {
+        result[0] = false;
+        result[1] = 'Ummm....do you even know how to play? You lose';
+    } else {
+        result[0] = false;
+	    result[1] = 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+    }
+
+    return result;
+
+}
+
+function game() {
+    let ready;
+    let overallResult = 0;
+    let message = '';
+
+    alert('Best of 5! Loser gets kicked in the nuts. Lets go');
+    
+    for (let i=0; i < 5; i++) {
+        ready = prompt("Choose your weapon","");
+        gameResult = playRound(ready);
+
+        console.log(gameResult[1]);
+
+        if (gameResult[0]) {
+            overallResult++
+        }
+    }
+
+    if (overallResult >= 3) {
+        message = 'Winner winner! Chicken dinner! You won ' + overallResult + '/5 games';
+    } else {
+        message = 'Sucked in loser! You only won ' + overallResult + '/5 games';
+    }
+
+    return message;
 
 }
