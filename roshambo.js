5b7792119ace8e67481633db0271c8bae5e60a54
Rosemary <rjingrey@gmail.com>
2022-08-29 08:28:02 +1000

    the UI so far

---
+++ roshambo.js
@@ -0,0 +1,28 @@
+function playRound() {
+    playerChoice = arguments;
+    computerChoice = getComputerChoice();
+    message = '';
+
+    if ((playerChoice == "rock" && computerChoice == "scissors") || 
+        (playerChoice == "scissors" && computerChoice == "paper") || 
+        (playerChoice == "paper" && computerChoice == "rock")) {
+            message = 'Congratulations! ' + playerChoice + ' beats ' + computerChoice;
+    } else if (playerChoice == computerChoice) {
+        message = 'Its a draw, nobody wins...';
+    } else {
+        message = 'You lose! ' + computerChoice + ' beats ' + playerChoice;
+    }
+
+}
+
+function getComputerChoice() {    
+    //randomly selects one of the defined options
+    let options = ["rock","paper","scissors"];
+    let choice = options[Math.floor(Math.random()*options.length)];
+    return choice;
+}
+
+const start = document.querySelector('#btnStart');
+start.addEventListener('click', () => {
+    alert('you clicked :o');
+});
--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-function getComputerChoice() {
-      
-    //console.log("I'll kick you in the nuts!");
-    
+function getComputerChoice() {    
     //randomly selects one of the defined options
     let options = ["rock","paper","scissors"];
     let choice = options[Math.floor(Math.random()*options.length)];
-
     return choice;
-
 }
 
 function playRound(playerSelection) {
