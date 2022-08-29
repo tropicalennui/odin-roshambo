805010769d25372c89314e4d3f244aa2895f3d81
Rosemary <rjingrey@gmail.com>
2022-08-29 21:11:57 +1000

    clicking start now displays the choices

--- roshambo.js
+++ roshambo.js
@@ -22,8 +22,41 @@ function getComputerChoice() {
     return choice;
 }
 
-const startBtn = document.querySelector('#startBtn');
-startBtn.addEventListener('click', () => {
+function newGame(){
+    const body = document.querySelector('body');
+        const gameWrapper = document.createElement('div');
+        gameWrapper.setAttribute('id','gameWrapper');
+    body.appendChild(gameWrapper);
+            const title = document.createElement('div');
+            title.classList.add('bangers');
+            title.setAttribute('id','title');
+            title.textContent = 'choose your weapon';
+        gameWrapper.appendChild(title);
+            const tiles = document.createElement('div');
+            tiles.setAttribute('id','tiles');
+        gameWrapper.appendChild(tiles);
+                const rock = document.createElement('div');
+                rock.setAttribute('id','rock');
+                rock.classList.add('tile');
+                rock.innerHTML = "<i class='fa-solid fa-hand-back-fist fa-flip-horizontal'></i>";
+            tiles.appendChild(rock);
+                const paper = document.createElement('div');
+                paper.classList.add('tile');
+                paper.setAttribute('id','paper');
+                paper.innerHTML = "<i class='fa-solid fa-hand'></i>";
+            tiles.appendChild(paper);
+                const scissors = document.createElement('div');
+                scissors.classList.add('tile');
+                scissors.setAttribute('id','scissors');
+                scissors.innerHTML = "<i class='fa-solid fa-hand-scissors'></i>";
+            tiles.appendChild(scissors);
+}
+
+function startGame(){
     const startGame = document.getElementById('startGame');
     startGame.remove();
-});
+    newGame();
+}
+
+const startBtn = document.querySelector('#startBtn');
+startBtn.addEventListener('click', startGame);
