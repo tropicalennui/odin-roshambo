10c2c3fa08bb52d6b51a87f1296a69363a832b2c
Rosemary <rjingrey@gmail.com>
2022-08-30 12:56:20 +1000

    simplified javascript by toggling classes to show or hide divs

--- roshambo.js
+++ roshambo.js
@@ -25,49 +25,32 @@ function getComputerChoice() {
 }
 
 function newGame(){
-    const body = document.querySelector('body');
-        const gameWrapper = document.createElement('div');
-        gameWrapper.setAttribute('id','gameWrapper');
-    body.appendChild(gameWrapper);
-            const title = document.createElement('div');
-            title.classList.add('bangers');
-            title.setAttribute('id','title');
-            title.textContent = 'choose your weapon';
-        gameWrapper.appendChild(title);
-            const tiles = document.createElement('div');
-            tiles.setAttribute('id','tiles');
-        gameWrapper.appendChild(tiles);
-                rock.setAttribute('id','rock');
-                rock.innerHTML = "<i class='fa-solid fa-hand-back-fist fa-flip-horizontal choice'></i>";
-            tiles.appendChild(rock);
-                paper.setAttribute('id','paper');
-                paper.innerHTML = "<i class='fa-solid fa-hand choice'></i>";
-            tiles.appendChild(paper);
-                scissors.setAttribute('id','scissors');
-                scissors.innerHTML = "<i class='fa-solid fa-hand-scissors choice'></i>";
-            tiles.appendChild(scissors);
+    const gameWrapper = document.getElementById('gameWrapper');
+    gameWrapper.classList.toggle("show");
+    gameWrapper.classList.toggle("hide");
 }
 
 function startGame(){
     const startGame = document.getElementById('startGame');
-    startGame.remove();
+    startGame.classList.toggle("show");
+    startGame.classList.toggle("hide");
     newGame();
 }
 
 const startBtn = document.querySelector('#startBtn');
 startBtn.addEventListener('click', startGame);
 
-const rock = document.createElement('div');
+const rock = document.getElementById('rock');
 rock.addEventListener('click',function(e) {
     playRound('rock');
 });
 
-const paper = document.createElement('div');
+const paper = document.getElementById('paper');
 paper.addEventListener('click',function(e) {
     playRound('paper');
 });
 
-const scissors = document.createElement('div');
+const scissors = document.getElementById('scissors');
 scissors.addEventListener('click',function(e) {
     playRound('scissors');
 });
